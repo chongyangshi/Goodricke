2d051955146c235c4428101b49eff65cfdee1942
C Shi <icydoge@users.noreply.github.com>
2015-07-07 01:57:42 +0800

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$reCaptchaSiteKey = ""; //Key and secret can be obtained at https://www.google.com/recaptcha/
-$reCaptchaSiteSecretKey = "";
-$CDNXForwardedFor = False; //Set this to True if your server is behind a CDN/Firewall service like CloudFlare.
-?>
